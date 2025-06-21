149fc5a2dbe84242105cd0dd8a8aa227af63bc7c
sairamadepu <sairam.adepu13@gmail.com>
2025-06-21 11:46:43 -0700

    updated InjectButton  in content

--- content.js
+++ content.js
@@ -1,7 +1,28 @@
 console.log("Email Writer Extension - content Script Loaded");
-function injectButton() {
+function findComposeToolbar() {
 
 }
+function createAIButton() {
+}
+
+function injectButton() {
+    const existingButton = document.querySelector('.ai-reply-button');
+    if (existingButton) existingButton.remove();
+    const toolbar = findComposeToolbar();
+    if (!toolbar) {
+        console.log("Toolbar not found");
+        return;
+    }
+    console.log("toolbar found, injecting button");
+
+    const button = createAIButton();
+    button.classList.add('ai-reply-button');
+    button.addEventListener('click', async () => {
+
+    });
+    toolbar.insertBefore(button, toolbar.firstChild);
+}
+
 const observer = new MutationObserver((mutations) => {
     for (const mutation of mutations) {
         const addedNodes = Array.from(mutation.addedNodes);
